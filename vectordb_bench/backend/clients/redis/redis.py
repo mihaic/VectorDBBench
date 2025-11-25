58a6de152f08c64264e9bad01a125c1bec4ee250
Mihai Capotă <mihai@mihaic.ro>
2025-11-24 17:08:55 -0800

    Fix Redis filtering

--- vectordb_bench/backend/clients/redis/redis.py
+++ vectordb_bench/backend/clients/redis/redis.py
@@ -5,9 +5,13 @@ from typing import Any
 import numpy as np
 import redis
 from redis.commands.search.field import NumericField, TagField, VectorField
-from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+try:
+    from redis.commands.search.indexDefinition import IndexDefinition, IndexType
+except ImportError:
+    from redis.commands.search.index_definition import IndexDefinition, IndexType
 from redis.commands.search.query import Query
 
+from vectordb_bench.backend.filter import Filter, FilterOp
 from ..api import DBCaseConfig, VectorDB
 
 log = logging.getLogger(__name__)
@@ -15,17 +19,30 @@ INDEX_NAME = "index"  # Vector Index Name
 
 
 class Redis(VectorDB):
+
+    supported_filter_types: list[FilterOp] = [
+        FilterOp.NonFilter,
+        FilterOp.NumGE,
+        FilterOp.StrEqual,
+    ]
+
     def __init__(
         self,
         dim: int,
         db_config: dict,
         db_case_config: DBCaseConfig,
         drop_old: bool = False,
+        with_scalar_labels: bool = False,
         **kwargs,
     ):
         self.db_config = db_config
         self.case_config = db_case_config
         self.collection_name = INDEX_NAME
+        self.with_scalar_labels = with_scalar_labels
+        self._filter = "*"
+        self._vector_field = "vector"
+        self._label_field = "label"
+        self._numeric_field = "metadata"
 
         # Create a redis connection, if db has password configured, add it to the connection here and in init():
         password = self.db_config["password"]
@@ -53,11 +70,10 @@ class Redis(VectorDB):
             # check to see if index exists
             conn.ft(INDEX_NAME).info()
         except Exception:
-            schema = (
-                TagField("id"),
-                NumericField("metadata"),
+            schema = [
+                NumericField(self._numeric_field),
                 VectorField(
-                    "vector",  # Vector Field Name
+                    self._vector_field,  # Vector Field Name
                     "HNSW",  # Vector Index Type: FLAT or HNSW
                     {
                         "TYPE": "FLOAT32",  # FLOAT32 or FLOAT64
@@ -67,7 +83,9 @@ class Redis(VectorDB):
                         "EF_CONSTRUCTION": self.case_config.index_param()["params"]["efConstruction"],
                     },
                 ),
-            )
+            ]
+            if self.with_scalar_labels:
+                schema.append(TagField(self._label_field))
 
             definition = IndexDefinition(index_type=IndexType.HASH)
 
@@ -102,6 +120,7 @@ class Redis(VectorDB):
         self,
         embeddings: list[list[float]],
         metadata: list[int],
+        labels_data: list[str] | None = None,
         **kwargs: Any,
     ) -> tuple[int, Exception]:
         """Insert embeddings into the database.
@@ -112,31 +131,44 @@ class Redis(VectorDB):
         try:
             with self.conn.pipeline(transaction=False) as pipe:
                 for i, embedding in enumerate(embeddings):
-                    ndarr_emb = np.array(embedding).astype(np.float32)
+                    ndarr_emb = np.array(embedding).astype(np.float32).tobytes()
+                    mapping={
+                        self._vector_field: ndarr_emb,
+                        self._numeric_field: metadata[i],
+                    }
+                    if self.with_scalar_labels:
+                        assert labels_data is not None
+                        mapping[self._label_field] = labels_data[i]
                     pipe.hset(
                         metadata[i],
-                        mapping={
-                            "id": str(metadata[i]),
-                            "metadata": metadata[i],
-                            "vector": ndarr_emb.tobytes(),
-                        },
+                        mapping=mapping,
                     )
                     # Execute the pipe so we don't keep too much in memory at once
                     if i % batch_size == 0:
-                        _ = pipe.execute()
+                        pipe.execute()
 
-                _ = pipe.execute()
+                pipe.execute()
                 result_len = i + 1
-        except Exception as e:
+        except redis.exceptions.RedisError as e:
             return 0, e
 
         return result_len, None
+
+    def prepare_filter(self, filters: Filter):
+        if filters.type == FilterOp.NonFilter:
+            self._filter = "*"
+        elif filters.type == FilterOp.NumGE:
+            self._filter = f"@{self._numeric_field}:[{filters.int_value} +inf]"
+        elif filters.type == FilterOp.StrEqual:
+            self._filter = f"@{self._label_field}:{{ {filters.label_value} }}"
+        else:
+            msg = f"Not support Filter for Redis - {filters}"
+            raise ValueError(msg)
 
     def search_embedding(
         self,
         query: list[float],
         k: int = 100,
-        filters: dict | None = None,
         timeout: int | None = None,
         **kwargs: Any,
     ) -> list[int]:
@@ -145,47 +177,9 @@ class Redis(VectorDB):
         query_vector = np.array(query).astype(np.float32).tobytes()
         ef_runtime = self.case_config.search_param()["params"]["ef"]
         query_obj = (
-            Query(f"*=>[KNN {k} @vector $vec EF_RUNTIME {ef_runtime} as score]")
-            .sort_by("score")
-            .return_fields("id", "score")
+            Query(f"{self._filter}=>[KNN {k} @{self._vector_field} $vec EF_RUNTIME {ef_runtime}]")
             .paging(0, k)
-            .dialect(2)
         )
         query_params = {"vec": query_vector}
-
-        if filters:
-            # benchmark test filters of format: {'metadata': '>=10000', 'id': 10000}
-            # gets exact match for id, and range for metadata if they exist in filters
-            id_value = filters.get("id")
-            metadata_value = filters.get("metadata")
-            if id_value and metadata_value:
-                query_obj = (
-                    Query(
-                        f"(@metadata:[{metadata_value} +inf] @id:{ {id_value} })=>[KNN {k} ",
-                        f"@vector $vec EF_RUNTIME {ef_runtime} as score]",
-                    )
-                    .sort_by("score")
-                    .return_fields("id", "score")
-                    .paging(0, k)
-                    .dialect(2)
-                )
-            elif id_value:
-                # gets exact match for id
-                query_obj = (
-                    Query(f"@id:{ {id_value} }=>[KNN {k} @vector $vec EF_RUNTIME {ef_runtime} as score]")
-                    .sort_by("score")
-                    .return_fields("id", "score")
-                    .paging(0, k)
-                    .dialect(2)
-                )
-            else:  # metadata only case, greater than or equal to metadata value
-                query_obj = (
-                    Query(f"@metadata:[{metadata_value} +inf]=>[KNN {k} @vector $vec EF_RUNTIME {ef_runtime} as score]")
-                    .sort_by("score")
-                    .return_fields("id", "score")
-                    .paging(0, k)
-                    .dialect(2)
-                )
         res = self.conn.ft(INDEX_NAME).search(query_obj, query_params)
-        # doc in res of format {'id': '9831', 'payload': None, 'score': '1.19209289551e-07'}
         return [int(doc["id"]) for doc in res.docs]
